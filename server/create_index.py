f3d407204b98bd8ae06a4712694cd324dcfc7701
Sam Yu <yudataguy@gmail.com>
2023-05-03 12:51:25 +0900

    easy heading fix

--- server/create_index.py
+++ server/create_index.py
@@ -4,11 +4,30 @@ from llama_index import GPTSimpleVectorIndex, MockEmbedding, ServiceContext
 
 staticPath = "static"
 
+def add_heading(markdown_text, heading_text, level=1):
+    """Add a heading to an existing Markdown text.
+
+    Args:
+        markdown_text (str): The existing Markdown text to which the heading will be added.
+        heading_text (str): The text for the heading.
+        level (int): The level of the heading (1 for the largest heading, 6 for the smallest).
+
+    Returns:
+        str: The Markdown text with the added heading.
+    """
+    heading = '#' * level
+    new_markdown_text = f"{heading} {heading_text}\n\n{markdown_text}"
+    return new_markdown_text
+
 
 def create_index(filepath, filename) -> int:
     # load data
     with open(filepath, "r", encoding="utf-8") as f:
         md_text = f.read()
+    
+    # simple fix heading issue
+    md_text = add_heading(md_text, filename)
+    
     html = markdown.markdown(
         md_text, extensions=["pymdownx.superfences", "tables", "pymdownx.details"]
     )
